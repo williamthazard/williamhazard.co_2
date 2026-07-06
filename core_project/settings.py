7a8f6d3931edb0381f18d8c7ecf66750650e21ad
williamthazard <williamthazard@pm.me>
2026-07-06 10:49:12 -0400

    fix: strip whitespace from ALLOWED_HOSTS in settings

--- core_project/settings.py
+++ core_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [host.strip() for host in os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',') if host.strip()]
 
 
 # Application definition
